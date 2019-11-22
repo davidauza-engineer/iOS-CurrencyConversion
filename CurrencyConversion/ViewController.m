2913e69228cf38dc820e56c1578092ed52ff7643
davidauza.engineer <davidauza01@gmail.com>
2019-11-22 07:34:45 -0500

    Add base logic

--- CurrencyConversion/ViewController.m
+++ CurrencyConversion/ViewController.m
@@ -7,12 +7,42 @@
 //
 
 #import "ViewController.h"
+#import <CurrencyRequest/CRCurrencyRequest.h>
+#import <CurrencyRequest/CRCurrencyResults.h>
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
+
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *currencyA;
+
+@property (weak, nonatomic) IBOutlet UILabel *currencyB;
+
+@property (weak, nonatomic) IBOutlet UILabel *currencyC;
 
 @end
 
 @implementation ViewController
+
+// This method is executed once the user presses the convert button.
+- (IBAction)buttonTapped:(id)sender {
+    self.convertButton.enabled = NO;
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+}
+
+// This method is called once the currency data is fetched.
+- (void)currencyRequest:(CRCurrencyRequest *)req retrievedCurrencies:(CRCurrencyResults *)currencies {
+    self.convertButton.enabled = YES;
+    double inputValue = [self.inputField.text doubleValue];
+    double euroValue = inputValue * currencies.EUR;
+    self.currencyA.text = [NSString stringWithFormat:@"%.2f", euroValue];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
